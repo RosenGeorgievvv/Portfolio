0091234428caadefb0ec7818ac7c4864b3ef54a1
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-05-30 14:47:22 +0300

    Update MainPage.jsx

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,10 +4,18 @@ import HiIcon from '../assets/hi.png';
 import LocationIcon from '../assets/place.png';
 import LinkedInIcon from '../assets/linkedinIco.png';
 import GitHubIcon from '../assets/githubIco.png';
+import HtmlIcon from '../assets/html.png';
+import CssIcon from '../assets/css.png';
+import JsIcon from '../assets/js.png';
+import ReactIcon from '../assets/react.png';
+import ThreeIcon from '../assets/three.png';
+import Avatar from '../assets/avatar.jpg';
+import BeeIcon from '../assets/bee.png';
+import BootstrapIcon from '../assets/bootstrap.png';
+
 
 import { OrbitControls, Sphere, MeshDiscardMaterial } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
-import { useState } from 'react';
 
 
 
@@ -18,9 +26,32 @@ function MainPage() {
     const [css, setCss] = useState('');
     const [js, setJs] = useState('');
     const [react, setReact] = useState('');
+    const [boot, setBoot] = useState('');
+    const [three, setThree] = useState('');
 
-
-    
+    const handleMouseEnter = (e) => {
+        if (e.target.id == "HTML") {
+            setHtml(true);
+        } else if (e.target.id == "CSS") {
+            setCss(true);
+        } else if (e.target.id == "JavaScript") {
+            setJs(true);
+        } else if (e.target.id == "Bootstrap") {
+            setBoot(true);
+        } else if (e.target.id == "React") {
+            setReact(true);
+        } else if (e.target.id == "Three.js") {
+            setThree(true);
+        };
+    }
+    const handleMouseLeave = () => {
+        setHtml();
+        setCss();
+        setJs();
+        setBoot();
+        setReact();
+        setThree();
+    };
 
     return (
         <div className='main' id='home'>
@@ -43,7 +74,10 @@ function MainPage() {
                     </a>
                     <div className='description'>Tech Stack:</div>
                     <div className='tech-stack-box'>
-                        <div className='tech-stack-single-box' id='HTML' onMouseEnter={handleMouseEnter}
+                        <div className='tech-stack-single-box' id='HTML' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                            <img src={HtmlIcon} className='tech-icon1' alt='html icon' />
+                            <span>{html ? "HTML" : ""}</span>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -51,4 +85,4 @@ function MainPage() {
     )
 }
 
-export default MainPage
+export default MainPage;
