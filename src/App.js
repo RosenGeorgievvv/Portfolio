589b12e844c799494988430548d16aa12f597a5d
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-05-31 14:55:20 +0300

    Projects component update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import MainPage from './pages/MainPage';
 import AboutMe from './components/AboutMe';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div className="app-container">
       <MainPage />
       <AboutMe />
+      <Projects />
     </div>
   );
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../styles/projects.css"
+
+function Projects() {
+    return (
+        <div className='section-projects' id='projects'>
+            <div className='projects-container'>
+                <div className='projects-title'>
+                    <h2>Projects</h2>
+                    <span>Below you can see my projects, check out the code on GitHub</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Projects
