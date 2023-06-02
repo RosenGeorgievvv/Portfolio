83fa8d2f841f54736630bf596804c0679224e02e
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-06-02 10:03:19 +0300

    Update ThirdProject.jsx

--- src/components/ThirdProject.jsx
+++ src/components/ThirdProject.jsx
@@ -26,7 +26,7 @@ function ThirdProject() {
   return (
     <div className='project-box'>
       <div className='third-leftSide'>
-        <img src={image} className='img' />
+        <img src={image} className='third-img' />
       </div>
       <div className='third-rightSide'>
         <h1>Calculator</h1>
